fd36084f118716a2375241d3fe2a1016f1f6cecc
thanhtp <thanhtp@rikkeisoft.com>
2018-10-04 16:19:58 +0700

    them phan add staff

--- source/src/main.js
+++ source/src/main.js
@@ -4,8 +4,23 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+import '../node_modules/adminbsb-materialdesign/plugins/bootstrap/css/bootstrap.css'
+import '../node_modules/adminbsb-materialdesign/plugins/node-waves/waves.css'
+import '../node_modules/adminbsb-materialdesign/plugins/animate-css/animate.css'
+import '../node_modules/adminbsb-materialdesign/plugins/morrisjs/morris.css'
+import '../node_modules/adminbsb-materialdesign/css/themes/all-themes.css'
+import './assets/css/style.css'
 
+import '../node_modules/jquery/src/jquery.js'
+import '../node_modules/adminbsb-materialdesign/plugins/jquery/jquery.min.js'
+import '../node_modules/adminbsb-materialdesign/plugins/bootstrap/js/bootstrap.js'
+import '../node_modules/adminbsb-materialdesign/plugins/bootstrap-select/js/bootstrap-select.js'
+import '../node_modules/adminbsb-materialdesign/plugins/jquery-slimscroll/jquery.slimscroll.js'
+import '../node_modules/adminbsb-materialdesign/plugins/node-waves/waves.js'
+import '../node_modules/adminbsb-materialdesign/plugins/autosize/autosize.js'
+import '../node_modules/adminbsb-materialdesign/js/admin.js'
+// import '../node_modules/adminbsb-materialdesign/js/pages/forms/basic-form-elements.js'
+Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
