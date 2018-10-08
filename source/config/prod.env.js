fb5e73cb18d0a0b86d407c0b97357bbd98ef08e4
HIEUTX-D5\hieutx <hieutx@rikkeisoft.com>
2018-10-08 08:07:34 +0700

    update config url

--- source/config/prod.env.js
+++ source/config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"http://2057da1f.ngrok.io/"'
+  // BASE_URL: '"http://2057da1f.ngrok.io/"'
+  BASE_URL: '"http://localhost:8085/"'
 }
