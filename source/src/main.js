aa2ee67f3e7ce8b3a099483493c9683f28a42e1a
thanhtp <thanhtp@rikkeisoft.com>
2018-10-09 17:13:32 +0700

    sua phan add employee

--- source/src/main.js
+++ source/src/main.js
@@ -9,7 +9,9 @@ import '../node_modules/adminbsb-materialdesign/plugins/node-waves/waves.css'
 import '../node_modules/adminbsb-materialdesign/plugins/animate-css/animate.css'
 import '../node_modules/adminbsb-materialdesign/plugins/morrisjs/morris.css'
 import '../node_modules/adminbsb-materialdesign/css/themes/all-themes.css'
+import '../node_modules/adminbsb-materialdesign/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css'
 import '../node_modules/adminbsb-materialdesign/plugins/sweetalert/sweetalert.css'
+import '../node_modules/adminbsb-materialdesign/plugins/bootstrap-select/css/bootstrap-select.css'
 import './assets/css/style.css'
 
 import '../node_modules/jquery/src/jquery.js'
@@ -21,6 +23,9 @@ import '../node_modules/adminbsb-materialdesign/plugins/node-waves/waves.js'
 import '../node_modules/adminbsb-materialdesign/plugins/autosize/autosize.js'
 import '../node_modules/adminbsb-materialdesign/js/admin.js'
 import '../node_modules/adminbsb-materialdesign/plugins/sweetalert/sweetalert.min.js'
+import '../node_modules/moment/moment.js'
+import '../node_modules/adminbsb-materialdesign/js/pages/forms/basic-form-elements.js'
+import '../node_modules/adminbsb-materialdesign/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js'
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
